6d6225ade117e3cead044fb39d63ff3627c53c26
GuillaumeMonnard <guillaume.monnard@studentfr.ch>
2025-01-27 17:04:01 +0100

    UpdateGIF au click

--- script.js
+++ script.js
@@ -69,8 +69,10 @@ const audioPlayer = (() => {
   const togglePlayPause = () => {
     if (audio.paused) {
       audio.play();
+      updateGif(true); // Appel à la fonction pour changer le GIF en mode "lecture"
     } else {
       audio.pause();
+      updateGif(false); // Appel à la fonction pour changer le GIF en mode "pause"
     }
   };
 
@@ -204,6 +206,9 @@ const audioPlayer = (() => {
       document.body.classList.remove("playing")
     );
     audio.addEventListener("ended", nextTrack);
+    // Ajout des écouteurs pour changer le GIF selon l'état de lecture
+    audio.addEventListener("play", () => updateGif(true));
+    audio.addEventListener("pause", () => updateGif(false));
 
     // Gestion des raccourcis clavier
     document.addEventListener("keydown", (e) => {
@@ -238,3 +243,13 @@ const audioPlayer = (() => {
       displayError("Erreur lors du chargement des informations de la musique.")
     );
 })();
+
+//Ajout de la fonction UPDATEGIF
+const updateGif = (isPlaying) => {
+  const gifElement = document.querySelector("#gif");
+  if (gifElement) {
+    gifElement.style.background = isPlaying
+      ? 'url("gifs/playing.gif") center / cover no-repeat'
+      : 'url("gifs/paused.gif") center / cover no-repeat';
+  }
+};
